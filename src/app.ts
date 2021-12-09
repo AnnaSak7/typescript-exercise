978f2d2c326b41e0b54272afc63ba88286c0cd3c
Anna Sakurai <zenasak@gmail.com>
2021-12-10 00:43:11 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,72 @@
+// // interfaces
+// interface IsPerson {
+//   name: string;
+//   age: number;
+//   speak(a: string): void;
+//   spend(a: number): number;
+// }
+
+// const me: IsPerson = {
+//   name: 'shaun',
+//   age: 30,
+//   speak(text: string): void {
+//     console.log(text);
+//   },
+//   spend(amount: number): number {
+//     console.log('I spend', amount);
+//     return amount;
+//   },
+// };
+
+// const greetPerson = (person: IsPerson) => {
+//   console.log('Hello ', person.name);
+// };
+
+import { Invoice } from './classes/invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+
+// docOne = new Invoice('mario', 'work on the mario website', 250);
+// docTwo = new Invoice('luigi', 'work on the mario website', 300);
+
+// const inOne = new Invoice('mario', 'work on the mario website', 250);
+// const inTwo = new Invoice('luigi', 'work on the mario website', 300);
+
+// let invoices: Invoice[] = [];
+// invoices.push(inOne);
+// invoices.push(inTwo);
+
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
+
+// console.log(invoices);
+
+//const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+//inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+  list.render(doc, type.value, 'end');
+});
